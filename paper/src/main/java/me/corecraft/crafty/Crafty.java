08fc376645fdbb4eb743b7b0ff63ffa9d40f0303
Ryder Belserion <no-reply@ryderbelserion.com>
2023-07-15 19:11:13 -0400

    Remove static getter

--- paper/src/main/java/me/corecraft/crafty/Crafty.java
+++ paper/src/main/java/me/corecraft/crafty/Crafty.java
@@ -4,14 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Crafty extends JavaPlugin {
 
-    private static Crafty plugin;
-
     @Override
     public void onEnable() {
-        plugin = this;
-    }
-
-    public static Crafty getPlugin() {
-        return plugin;
+        getLogger().info("Guten Tag!");
     }
 }
