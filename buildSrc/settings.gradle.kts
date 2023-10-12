c833aea234b3fd729935a2d48b0b5f1683a40831
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-11 22:50:40 -0400

    remove gamerule command

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -2,6 +2,8 @@ dependencyResolutionManagement {
     repositories {
         maven("https://repo.papermc.io/repository/maven-public/")
 
+        maven("https://maven.fabricmc.net/")
+
         gradlePluginPortal()
     }
 }
