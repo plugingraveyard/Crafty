2b2a5ad7a73897e7ba4cf4e5afe67f9c5bcf06a4
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-09 01:06:18 -0500

    Fix errors

--- paper/src/main/java/com/ryderbelserion/crafty/paper/api/managers/CommandManager.java
+++ paper/src/main/java/com/ryderbelserion/crafty/paper/api/managers/CommandManager.java
@@ -5,9 +5,14 @@ import com.ryderbelserion.crafty.paper.commands.CraftyCommand;
 import com.ryderbelserion.crafty.paper.commands.handlers.ArgumentRelations;
 import com.ryderbelserion.crafty.paper.commands.handlers.MiscRelations;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
+import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class CommandManager {
 
@@ -18,10 +23,22 @@ public class CommandManager {
     private final BukkitCommandManager<CommandSender> bukkitCommandManager = BukkitCommandManager.create(this.plugin);
 
     public void load() {
-        this.bukkitCommandManager.registerCommand(new CraftyCommand());
-
         new MiscRelations().build();
         new ArgumentRelations().build();
+
+        this.bukkitCommandManager.registerCommand(new CraftyCommand());
+
+        this.bukkitCommandManager.registerSuggestion(SuggestionKey.of("online-players"), (sender, context) -> this.plugin.getServer().getOnlinePlayers().stream().map(Player::getName).toList());
+
+        this.bukkitCommandManager.registerSuggestion(SuggestionKey.of("worlds"), (sender, context) -> this.plugin.getServer().getWorlds().stream().map(World::getName).toList());
+
+        this.bukkitCommandManager.registerSuggestion(SuggestionKey.of("numbers"), (sender, context) -> {
+            List<String> numbers = new ArrayList<>();
+
+            for (int i = 1; i <= 100; i++) numbers.add(String.valueOf(i));
+
+            return numbers;
+        });
     }
 
     @NotNull
