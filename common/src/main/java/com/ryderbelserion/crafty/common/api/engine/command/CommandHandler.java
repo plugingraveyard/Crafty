975b0e77e47382b88365696aef73f31d6399f1cb
Ryder Belserion <no-reply@ryderbelserion.com>
2024-01-11 23:33:57 -0500

    Make the config options useful

--- common/src/main/java/com/ryderbelserion/crafty/common/api/engine/command/CommandHandler.java
+++ common/src/main/java/com/ryderbelserion/crafty/common/api/engine/command/CommandHandler.java
@@ -1,12 +1,20 @@
 package com.ryderbelserion.crafty.common.api.engine.command;
 
+import ch.jalu.configme.SettingsManager;
 import cloud.commandframework.Command;
 import cloud.commandframework.CommandManager;
 import cloud.commandframework.meta.CommandMeta;
 import cloud.commandframework.minecraft.extras.AudienceProvider;
 import cloud.commandframework.minecraft.extras.MinecraftHelp;
+import com.ryderbelserion.crafty.common.api.CraftyPlugin;
 import com.ryderbelserion.crafty.common.api.engine.command.types.Sender;
+import com.ryderbelserion.crafty.common.api.interfaces.AbstractPlugin;
+import com.ryderbelserion.crafty.common.config.types.Config;
+import com.ryderbelserion.crafty.common.utils.ColorUtils;
+import net.kyori.adventure.text.format.TextColor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public abstract class CommandHandler {
 
@@ -26,6 +34,10 @@ public abstract class CommandHandler {
                 .permission("crafty.access")
                 .meta(CommandMeta.DESCRIPTION, "The base command for Crafty.")
                 .handler(context -> {
+                    AbstractPlugin plugin = CraftyPlugin.get();
+
+                    SettingsManager config = plugin.getConfig();
+
                     MinecraftHelp<Sender> help = new MinecraftHelp<>(
                             "/crafty help",
                             AudienceProvider.nativeAudience(),
@@ -35,6 +47,15 @@ public abstract class CommandHandler {
                     help.setMessage("help", "Crafty Help");
 
                     help.setMaxResultsPerPage(config.getProperty(Config.help_max_results_per_page));
+
+                    help.setHelpColors(MinecraftHelp.HelpColors.of(
+                            ColorUtils.getColor(config.getProperty(Config.help_primary_color)),
+                            ColorUtils.getColor(config.getProperty(Config.help_highlight_color)),
+                            ColorUtils.getColor(config.getProperty(Config.help_alternate_highlight_color)),
+                            ColorUtils.getColor(config.getProperty(Config.help_text_color)),
+                            ColorUtils.getColor(config.getProperty(Config.help_accent_color))
+                    ));
+
                     help.queryCommands(context.getOrDefault("query", ""), context.getSender());
 
                    //TODO() Add some type of wrapper to open gui's
