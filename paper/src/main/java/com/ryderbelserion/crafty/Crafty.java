20b63d93a12af9e1b361f1e67bc85a64024678a4
Ryder Belserion <no-reply@ryderbelserion.com>
2024-01-12 18:45:17 -0500

    Clean up onEnable

--- paper/src/main/java/com/ryderbelserion/crafty/Crafty.java
+++ paper/src/main/java/com/ryderbelserion/crafty/Crafty.java
@@ -1,13 +1,9 @@
 package com.ryderbelserion.crafty;
 
 import com.ryderbelserion.cluster.ClusterFactory;
-import com.ryderbelserion.cluster.utils.AdvUtils;
 import com.ryderbelserion.crafty.api.PaperAbstractPlugin;
 import com.ryderbelserion.crafty.api.command.PaperCommandManager;
-import com.ryderbelserion.crafty.commands.subs.TestCommand;
 import com.ryderbelserion.crafty.common.config.types.Config;
-import com.ryderbelserion.crafty.modules.HitDelayModule;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Crafty extends JavaPlugin {
@@ -25,23 +21,19 @@ public class Crafty extends JavaPlugin {
         this.cluster = new ClusterFactory(this, this.plugin.getConfig().getProperty(Config.verbose_logging));
         this.cluster.enable();
 
-        PaperCommandManager manager;
-
         try {
-            manager = new PaperCommandManager(this);
+            new PaperCommandManager(this);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         // Let's see if this works.
-        new TestCommand(manager.getHandler()).register();
-
-        printModules();
+        // new ItemCommand(manager.getHandler()).register();
     }
 
     @Override
     public void onDisable() {
-        this.plugin.disable();
+        if (this.plugin != null) this.plugin.stop();
 
         // Shut down cluster factory.
         if (this.cluster != null) this.cluster.disable();
