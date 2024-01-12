d09e1b23881997c73e39f1024b049025a5cb6f64
Ryder Belserion <no-reply@ryderbelserion.com>
2024-01-12 18:43:25 -0500

    Remove module loader

--- paper/src/main/java/com/ryderbelserion/crafty/Crafty.java
+++ paper/src/main/java/com/ryderbelserion/crafty/Crafty.java
@@ -2,7 +2,6 @@ package com.ryderbelserion.crafty;
 
 import com.ryderbelserion.cluster.ClusterFactory;
 import com.ryderbelserion.cluster.utils.AdvUtils;
-import com.ryderbelserion.cluster.utils.modules.ModuleLoader;
 import com.ryderbelserion.crafty.api.PaperAbstractPlugin;
 import com.ryderbelserion.crafty.api.command.PaperCommandManager;
 import com.ryderbelserion.crafty.commands.subs.TestCommand;
@@ -15,8 +14,6 @@ public class Crafty extends JavaPlugin {
 
     private PaperAbstractPlugin plugin;
 
-    private ModuleLoader moduleLoader;
-
     private ClusterFactory cluster;
 
     @Override
@@ -27,12 +24,6 @@ public class Crafty extends JavaPlugin {
         // Enable cluster factory.
         this.cluster = new ClusterFactory(this, this.plugin.getConfig().getProperty(Config.verbose_logging));
         this.cluster.enable();
-
-        // Load modules.
-        this.moduleLoader = new ModuleLoader();
-        this.moduleLoader.addModule(new HitDelayModule(this));
-
-        this.moduleLoader.load();
 
         PaperCommandManager manager;
 
@@ -56,27 +47,7 @@ public class Crafty extends JavaPlugin {
         if (this.cluster != null) this.cluster.disable();
     }
 
-    public void printModules() {
-        String prefix = this.plugin.getConfig().getProperty(Config.console_prefix);
-
-        ConsoleCommandSender sender = getServer().getConsoleSender();
-
-        this.moduleLoader.getModules().forEach(module -> {
-            String misc = prefix + "<bold><gold>" + module.getModuleName() + "</gold>";
-
-            if (module.isEnabled()) {
-                sender.sendMessage(AdvUtils.parse(misc + " <green>ENABLED</green>."));
-            } else {
-                sender.sendMessage(AdvUtils.parse(misc + " <red>NOT ENABLED.</red>"));
-            }
-        });
-    }
-
     public ClusterFactory getCluster() {
         return this.cluster;
-    }
-
-    public ModuleLoader getModuleLoader() {
-        return this.moduleLoader;
     }
 }
